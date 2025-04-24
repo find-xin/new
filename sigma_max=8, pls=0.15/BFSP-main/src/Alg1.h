fd5e373ea6e7f2dd4e49e8fe48433157632b2484
find-xin <3620937088@qq.com>
2025-04-24 10:59:48 +0800

    Initial commit

---
+++ sigma_max=8, pls=0.15/BFSP-main/src/Alg1.h
@@ -0,0 +1,15 @@
+//
+// Created by DELL on 24-11-20.
+//
+
+#ifndef ALG1_H
+#define ALG1_H
+
+#include <vector>
+#include "GlobalData.h"
+#define inf INT_MAX
+// 声明函数
+std::vector<std::vector<int> > ALG1(int N0, int lambda = globalData.lambda, int x = 5);
+
+
+#endif //ALG1_H
